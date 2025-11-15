cef4c3aecb9f1635128b512d3becd8436e5e6b77
annemariv <annemari.velling@gmail.com>
2025-11-15 08:53:44 +0200

    Add update and delete
    Positive, negative data test
    Some fixes for add and update methods
    Getting id from URL method fix

--- RealEstateTest.cs
+++ RealEstateTest.cs
@@ -1,5 +1,7 @@
+using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 
 namespace ShopTARge24TestingNUnit
 {
@@ -12,13 +14,15 @@ namespace ShopTARge24TestingNUnit
         public void Initialize()
         {
             driver = new FirefoxDriver();
+            driver.Url = "https://localhost:7282";
         }
 
-        [Test]
+        //----------------POSITIVE-----------------
+
+        //Add
+        [Test, Order(1)]
         public void TestRealEstateAddPositive()
         {
-            driver.Url = "https://localhost:7282";
-
             IWebElement idOfLinkElement = driver.FindElement(By.Id("realestate"));
             idOfLinkElement.Click();
 
@@ -29,37 +33,144 @@ namespace ShopTARge24TestingNUnit
 
             InsertRealEstateDataPositive(driver);
 
+
             IWebElement createSubmitButton = driver.FindElement(By.Id("testICreateSubmit"));
             createSubmitButton.Click();
-            Thread.Sleep(500);
+            Thread.Sleep(1000);
 
-            ICollection<IWebElement> elementsToCheck = driver.FindElements(By.Id("testIdTableLocation"));
+            //ICollection<IWebElement> elementsToCheck = driver.FindElements(By.Id("testIdTableLocation"));
+            IWebElement locationElement = driver.FindElement(By.Id("testIdTableLocation"));
+            IWebElement roomNumberElement = driver.FindElement(By.Id("testIdTableRoomNumber"));
+            IWebElement areaElement = driver.FindElement(By.Id("testIdTableArea"));
+            IWebElement buildingTypeElement = driver.FindElement(By.Id("testIdBuildingType"));
 
+            Assert.That(locationElement.Text, Is.EqualTo("TestLocation_01"));
+            Assert.That(roomNumberElement.Text, Is.EqualTo("7"));
+            Assert.That(areaElement.Text, Is.EqualTo("321"));
+            Assert.That(buildingTypeElement.Text, Is.EqualTo("apartment"));
 
-            IWebElement idOfTestData = driver.FindElement(By.Id("testIdTableLocation"));
-            var dataInIndex = idOfTestData.Text;
-
-            IWebElement idOfTestData2 = driver.FindElement(By.Id("testIdTableRoomNumber"));
-            var dataInIndex2 = idOfTestData2.Text;
-
-            IWebElement idOfTestData3 = driver.FindElement(By.Id("testIdArea"));
-            var dataInIndex3 = idOfTestData3.Text;
-
-            IWebElement idOfTestData4 = driver.FindElement(By.Id("testIdArea"));
-            var dataInIndex4 = idOfTestData4.Text;
-
-            Assert.That(dataInIndex, Is.EqualTo("TestLocation_01"));
-            Assert.That(dataInIndex2, Is.EqualTo("7"));
-            Assert.That(dataInIndex3, Is.EqualTo("321"));
-            Assert.That(dataInIndex4, Is.EqualTo("apartment"));
             Console.WriteLine("Test passed");
+        }
+        
+        private void InsertRealEstateDataPositive(IWebDriver driver)
+        {            
+            IWebElement idOfLocation = driver.FindElement(By.Id("testIdLocation"));
+            idOfLocation.SendKeys("TestLocation_01");
+
+            IWebElement idOfRoomNr = driver.FindElement(By.Id("testIdRoomNumber"));
+            idOfRoomNr.SendKeys("7");
+
+            IWebElement idOfArea = driver.FindElement(By.Id("testIdArea"));
+            idOfArea.Clear(); //kui vaja väli enne puhastada, default value 
+            idOfArea.SendKeys("321");
+
+            IWebElement idOfBuildingType = driver.FindElement(By.Id("testIdBuildingType"));
+            idOfBuildingType.SendKeys("apartment");
 
         }
 
-        [Test]
+
+        //get id from URL
+        private string GetIdFromUrl(IWebDriver driver)
+        {
+            string url = driver.Url;
+            return url.Substring(url.LastIndexOf('/') + 1);
+        }
+
+        //Update
+        [Test, Order(2)]
+        public void TestRealEstateUpdatePositive()
+        {
+            driver.Navigate().GoToUrl("https://localhost:7282");
+            Thread.Sleep(500);
+
+            driver.FindElement(By.Id("realestate")).Click();
+            Thread.Sleep(1000);
+
+            IWebElement updateElementButton = driver.FindElement(By.Id("testIdUpdate"));
+            updateElementButton.Click();
+            Thread.Sleep(1000);
+
+            string id = GetIdFromUrl(driver);
+            Console.WriteLine("ID from URL: " + id);
+
+            UpdateRealEstateDataPositive(driver);
+            Thread.Sleep(1000);
+
+            IWebElement updateSubmitButton = driver.FindElement(By.Id("testIdUpdateSubmit"));
+            updateSubmitButton.Click();
+            Thread.Sleep(1000); 
+
+            driver.FindElement(By.Id("realestate")).Click();
+            Thread.Sleep(1000);
+
+            IWebElement dataInIndexElement = driver.FindElement(By.Id("testIdTableLocation"));
+            IWebElement dataInIndex2Element = driver.FindElement(By.Id("testIdTableRoomNumber"));
+            IWebElement dataInIndex3Element = driver.FindElement(By.Id("testIdTableArea"));
+            IWebElement dataInIndex4Element = driver.FindElement(By.Id("testIdBuildingType"));
+
+            Assert.That(dataInIndexElement.Text, Is.EqualTo("TestLocation_02"));
+            Assert.That(dataInIndex2Element.Text, Is.EqualTo("10"));
+            Assert.That(dataInIndex3Element.Text, Is.EqualTo("123"));
+            Assert.That(dataInIndex4Element.Text, Is.EqualTo("hotel"));
+
+            Console.WriteLine("Update test passed");
+        }
+        
+        private void UpdateRealEstateDataPositive(IWebDriver driver)
+        {           
+            IWebElement idOfLocation = driver.FindElement(By.Id("testIdLocation"));
+            idOfLocation.Clear();
+            idOfLocation.SendKeys("TestLocation_02");
+
+            IWebElement idOfRoomNr = driver.FindElement(By.Id("testIdRoomNumber"));
+            idOfRoomNr.Clear();
+            idOfRoomNr.SendKeys("10");
+
+            IWebElement idOfArea = driver.FindElement(By.Id("testIdArea"));
+            idOfArea.Clear(); //kui vaja väli enne puhastada, default value 
+            idOfArea.SendKeys("123");
+
+            IWebElement idOfBuildingType = driver.FindElement(By.Id("testIdBuildingType"));
+            idOfBuildingType.Clear();
+            idOfBuildingType.SendKeys("hotel");
+        }
+
+        //Delete after all pos and negative have tested
+        [Test, Order(5)]
+        public void TestRealEstateDeletePositive()
+        {
+            driver.Navigate().GoToUrl("https://localhost:7282");
+            driver.FindElement(By.Id("realestate")).Click();
+            Thread.Sleep(500);
+
+            IWebElement firstLocation = driver.FindElement(By.Id("testIdTableLocation"));
+            string locationToDelete = firstLocation.Text;
+
+            driver.FindElement(By.Id("testIdDelete")).Click();
+            Thread.Sleep(500);
+
+            driver.FindElement(By.Id("testSubmitDelete")).Click();
+            Thread.Sleep(500);
+
+            driver.FindElement(By.Id("realestate")).Click();
+            Thread.Sleep(500);
+
+            var locations = driver.FindElements(By.Id("testIdTableLocation"));
+            bool stillExists = locations.Any(e => e.Text == locationToDelete);
+            Assert.That(stillExists, Is.False);
+
+            Console.WriteLine("Delete test passed");
+        }
+
+
+
+        //----------------NEGATIVE-------------------
+
+        //Create
+        [Test, Order(3)]
         public void TestRealEstateAddNegative()
         {
-            driver.Url = "https://localhost:7282";
 
             IWebElement idOfLinkElement = driver.FindElement(By.Id("realestate"));
             idOfLinkElement.Click();
@@ -75,41 +186,25 @@ namespace ShopTARge24TestingNUnit
             createSubmitButton.Click();
             Thread.Sleep(3000);
 
-            //ICollection<IWebElement> elementsToCheck = driver.FindElements(By.Id("testIdTableLocation"));
+            IWebElement locationErrorEl = driver.FindElement(By.Id("testIdTableLocation"));
+            IWebElement roomNumberErrorEl = driver.FindElement(By.Id("testIdTableRoomNumber"));
+            IWebElement areaErrorEl = driver.FindElement(By.Id("testIdTableArea"));
+            IWebElement buildingTypeErrorEl = driver.FindElement(By.Id("testIdBuildingType"));
 
+            Assert.That(locationErrorEl.Text, Is.EqualTo("Location must be letters only"));
+            Assert.That(roomNumberErrorEl.Text, Is.EqualTo("Room number must be numeric"));
+            Assert.That(areaErrorEl.Text, Is.EqualTo("Area must be numeric"));
+            Assert.That(buildingTypeErrorEl.Text, Is.EqualTo("Building type must be text"));
 
-            //IWebElement idOfTestData = driver.FindElement(By.Id("testIdTableLocation"));
-            //var nameInIndex = idOfTestData.Text;         
-
-            //IWebElement idOfTestData2 = driver.FindElement(By.Id("testIdTableRoomNumber"));
-            //var nameInIndex2 = idOfTestData2.Text;
-
-            //Assert.That(nameInIndex, Is.EqualTo("TestLocation_01"));
-            //Assert.That(nameInIndex2, Is.EqualTo("7"));
-            //Console.WriteLine("Test passed");
-
+            Console.WriteLine("Negative add test passed");
         }
 
-        private void InsertRealEstateDataPositive(IWebDriver driver)
-        {
-            IWebElement idOfLocation = driver.FindElement(By.Id("testIdLocation"));
-            idOfLocation.SendKeys("TestLocation_01");
-
-            IWebElement idOfRoomNr = driver.FindElement(By.Id("testIdRoomNumber"));
-            idOfRoomNr.SendKeys("7");
-
-            IWebElement idOfArea = driver.FindElement(By.Id("testIdArea"));
-            idOfArea.Clear(); //kui vaja väli enne puhastada, default value 
-            idOfArea.SendKeys("321");
-
-            IWebElement idOfBuildingType = driver.FindElement(By.Id("testIdBuildingType"));
-            idOfBuildingType.SendKeys("apartment");
-
-        }
+       
         private void InsertRealEstateDataNegative(IWebDriver driver)
         {
             IWebElement idOfLocation = driver.FindElement(By.Id("testIdLocation"));
             idOfLocation.SendKeys("1234");
+
             IWebElement idOfRoomNr = driver.FindElement(By.Id("testIdRoomNumber"));
             idOfRoomNr.SendKeys("Twelve");
 
@@ -119,6 +214,61 @@ namespace ShopTARge24TestingNUnit
             IWebElement idOfBuildingType = driver.FindElement(By.Id("testIdBuildingType"));
             idOfBuildingType.SendKeys("9876");
 
+        }
+
+        //Update
+        [Test, Order(4)]
+        public void TestRealEstateUpdateNegative()
+        {
+            driver.Navigate().GoToUrl("https://localhost:7282");
+            Thread.Sleep(500);
+
+            driver.FindElement(By.Id("realestate")).Click();
+            Thread.Sleep(500);
+
+            IWebElement updateElementButton = driver.FindElement(By.Id("testIdUpdate"));
+            updateElementButton.Click();
+            Thread.Sleep(500);
+
+            string id = GetIdFromUrl(driver);
+            Console.WriteLine("ID from URL: " + id);
+
+            InsertRealEstateDataNegativeForUpdate(driver);
+
+            IWebElement updateSubmitButton = driver.FindElement(By.Id("testIdUpdateSubmit"));
+            updateSubmitButton.Click();
+            Thread.Sleep(500);
+
+            IWebElement locationErrorEl = driver.FindElement(By.Id("testIdTableLocation"));
+            IWebElement roomNumberErrorEl = driver.FindElement(By.Id("testIdTableRoomNumber"));
+            IWebElement areaErrorEl = driver.FindElement(By.Id("testIdTableArea"));
+            IWebElement buildingTypeErrorEl = driver.FindElement(By.Id("testIdBuildingType"));
+
+            Assert.That(locationErrorEl.Text, Is.EqualTo("Location must be letters only"));
+            Assert.That(roomNumberErrorEl.Text, Is.EqualTo("Room number must be numeric"));
+            Assert.That(areaErrorEl.Text, Is.EqualTo("Area must be numeric"));
+            Assert.That(buildingTypeErrorEl.Text, Is.EqualTo("Building type must be text"));
+
+            Console.WriteLine("Negative update test passed");
+        }
+
+        private void InsertRealEstateDataNegativeForUpdate(IWebDriver driver)
+        {
+            IWebElement locationField = driver.FindElement(By.Id("testIdLocation"));
+            locationField.Clear();
+            locationField.SendKeys("1234");
+
+            IWebElement roomNumberField = driver.FindElement(By.Id("testIdRoomNumber"));
+            roomNumberField.Clear();
+            roomNumberField.SendKeys("Twelve");
+
+            IWebElement areaField = driver.FindElement(By.Id("testIdArea"));
+            areaField.Clear();
+            areaField.SendKeys("Something");
+
+            IWebElement buildingTypeField = driver.FindElement(By.Id("testIdBuildingType"));
+            buildingTypeField.Clear();
+            buildingTypeField.SendKeys("9876");
         }
 
         [TearDown]
